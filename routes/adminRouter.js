6e6943fb7ca4a9fc6a198723a743d9212aa9f101
Tri Nguyen <nguyenduongtri0503@gmail.com>
2020-01-09 01:11:03 +0700

    admin book management (view list + search by title) + paginate

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,8 +1,35 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('admin-home');
+router.get('/', (req, res, next) => {
+    if(req.query.limit == null || isNaN(req.query.limit))
+    {
+        req.query.limit = 6;
+    }
+
+    if(req.query.page == null || isNaN(req.query.page))
+    {
+        req.query.page = 1;
+    }
+
+    if (req.query.search == null || req.query.search.trim() == '') {
+        req.query.search = '';
+    }
+
+    var request = req.query;
+    let adminController = require('../controllers/adminController');
+    adminController
+        .getAll(request)
+        .then(data => {
+            res.locals.adminBooks = data.rows;
+            res.locals.pagination = {
+                page: parseInt(req.query.page),
+                limit: parseInt(req.query.limit),
+                totalRows: data.count
+            };
+            res.render('admin-home');
+        })
+        .catch(error => next(error));
 });
 
 module.exports = router;
